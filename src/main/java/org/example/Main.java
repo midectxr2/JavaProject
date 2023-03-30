2db5e559a30b73e824a8fa7dc251b09e32b8b49b
Thomas Tomsen <thomas.tomsen59570@gmail.com>
2023-03-30 17:08:39 +0200

    Touche pas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
      */
     public static void main(String[] args) {
         int i=1;
-        String j="efface pas grs con";
+        String j="efface pas gros con";
         System.out.println("Hello world!" + i+ j);
     }
 
