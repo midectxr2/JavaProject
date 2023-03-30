06b482fdb2e751ba7affbfe452c41a9e82adb128
midectxr2 <decorteemile840@gmail.com>
2023-03-30 11:58:57 +0200

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Main {
+    /**
+     * Methode main de test
+     *
+     * @param args argument de test
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
